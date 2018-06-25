b5f372513f8a46f97ae8d3213f4998bbb1de4665
anton.vasyliev <antonvas@gmail.com>
2018-06-25 13:40:53 +0300

    CB-1923 - EntityConsumer to listen for multiple entities

---
+++ test/Listener/CallbackEntityListenerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Partnermarketing\Queue\Test\Listener;
+
+use Partnermarketing\Queue\Listener\CallbackEntityListener;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests that the CallbackEntityListener works as expected
+ */
+class CallbackEntityListenerTest extends TestCase
+{
+    /**
+     * The listener we are testing
+     */
+    private $listener;
+
+    /**
+     * Mock callback to set callback expectations
+     */
+    private $mockCallback;
+
+    /**
+     * Sets up the test
+     */
+    public function setUp()
+    {
+        $this->mockCallback = $this->createPartialMock('stdClass', ['callback']);
+
+        $this->listener = new CallbackEntityListener(
+            [$this->mockCallback, 'callback']
+        );
+    }
+
+    /**
+     * Tests that execute() calls the callback we set in the constructor
+     */
+    public function testWithEntity()
+    {
+        $event = ['1', '2', '3'];
+
+        $this->mockCallback->expects($this->once())
+            ->method('callback')
+            ->with($event);
+
+
+        $this->listener->withEntity($event);
+    }
+}
